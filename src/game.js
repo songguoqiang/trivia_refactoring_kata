1eb845904f36ee658c975b4034e3279f60b84dfb
songguoqiang <songguoq@gmail.com>
2018-08-18 10:41:58 +0800

    refactoring: extract all console.log to one log function

--- src/game.js
+++ src/game.js
@@ -54,46 +54,40 @@ module.exports = function Game() {
     purses[playersCount - 1] = 0;
     inPenaltyBox[playersCount - 1] = false;
 
-    console.log(playerName + " was added");
-    console.log("They are player number " + players.length);
+    log(playerName + " was added");
+    log("They are player number " + players.length);
 
     return true;
   };
 
   const askQuestion = function() {
-    if (currentCategory() == POP_CATEGORY) console.log(popQuestions.shift());
-    if (currentCategory() == SCIENCE_CATEGORY)
-      console.log(scienceQuestions.shift());
-    if (currentCategory() == SPORTS_CATEGORY)
-      console.log(sportsQuestions.shift());
-    if (currentCategory() == ROCK_CATEGORY) console.log(rockQuestions.shift());
+    if (currentCategory() == POP_CATEGORY) log(popQuestions.shift());
+    if (currentCategory() == SCIENCE_CATEGORY) log(scienceQuestions.shift());
+    if (currentCategory() == SPORTS_CATEGORY) log(sportsQuestions.shift());
+    if (currentCategory() == ROCK_CATEGORY) log(rockQuestions.shift());
   };
 
   this.roll = function(roll) {
-    console.log(players[currentPlayer] + " is the current player");
-    console.log("They have rolled a " + roll);
+    log(players[currentPlayer] + " is the current player");
+    log("They have rolled a " + roll);
 
     if (inPenaltyBox[currentPlayer]) {
       if (roll % 2 != 0) {
         isGettingOutOfPenaltyBox = true;
 
-        console.log(
-          players[currentPlayer] + " is getting out of the penalty box"
-        );
+        log(players[currentPlayer] + " is getting out of the penalty box");
         places[currentPlayer] = places[currentPlayer] + roll;
         if (places[currentPlayer] > 11) {
           places[currentPlayer] = places[currentPlayer] - 12;
         }
 
-        console.log(
+        log(
           players[currentPlayer] + "'s new location is " + places[currentPlayer]
         );
-        console.log("The category is " + currentCategory());
+        log("The category is " + currentCategory());
         askQuestion();
       } else {
-        console.log(
-          players[currentPlayer] + " is not getting out of the penalty box"
-        );
+        log(players[currentPlayer] + " is not getting out of the penalty box");
         isGettingOutOfPenaltyBox = false;
       }
     } else {
@@ -102,10 +96,10 @@ module.exports = function Game() {
         places[currentPlayer] = places[currentPlayer] - 12;
       }
 
-      console.log(
+      log(
         players[currentPlayer] + "'s new location is " + places[currentPlayer]
       );
-      console.log("The category is " + currentCategory());
+      log("The category is " + currentCategory());
       askQuestion();
     }
   };
@@ -113,9 +107,9 @@ module.exports = function Game() {
   this.wasCorrectlyAnswered = function() {
     if (inPenaltyBox[currentPlayer]) {
       if (isGettingOutOfPenaltyBox) {
-        console.log("Answer was correct!!!!");
+        log("Answer was correct!!!!");
         purses[currentPlayer] += 1;
-        console.log(
+        log(
           players[currentPlayer] +
             " now has " +
             purses[currentPlayer] +
@@ -133,10 +127,10 @@ module.exports = function Game() {
         return true;
       }
     } else {
-      console.log("Answer was correct!!!!");
+      log("Answer was correct!!!!");
 
       purses[currentPlayer] += 1;
-      console.log(
+      log(
         players[currentPlayer] +
           " now has " +
           purses[currentPlayer] +
@@ -153,8 +147,8 @@ module.exports = function Game() {
   };
 
   this.wrongAnswer = function() {
-    console.log("Question was incorrectly answered");
-    console.log(players[currentPlayer] + " was sent to the penalty box");
+    log("Question was incorrectly answered");
+    log(players[currentPlayer] + " was sent to the penalty box");
     inPenaltyBox[currentPlayer] = true;
 
     currentPlayer += 1;
@@ -162,3 +156,7 @@ module.exports = function Game() {
     return true;
   };
 };
+
+function log(message) {
+  console.log(message);
+}
